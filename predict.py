193794b2a88ad7397b7d54a2921253f5aa639aca
wanghs <1344754848@qq.com>
2018-11-05 21:08:27 +0800

    开始

---
+++ predict.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+#读取数据
+x = pd.read_csv("train.csv")
+
+#数据预处理
+#包含缺失值的处理，对一些状态column的Durex编码，特征选择（降维）？，数据分割
+
+
+#训练   
+
+#输出
